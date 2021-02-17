925b2b91b338fc644db4499ff6b88cac469a9efb
Eduardo Steffens <eduardoks1998@gmail.com>
2021-02-17 11:02:17 -0300

    database update

--- database/database.sql
+++ database/database.sql
@@ -12,50 +12,81 @@ CREATE TABLE game(
 
 RENAME TABLE game to games;
 
+--SODIESEL
 CREATE TABLE clientes(
     cli_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    cli_cnpj INT(11),
-    cli_motorista VARCHAR(255),
-    cli_motor_telefone INT(11),
+    cli_cnpj VARCHAR(255),
     cli_pj_razao_social	VARCHAR(255),
-    cli_pj_telefone	INT(11),
-    cli_pj_ie INT(11),
-    cli_cpf	INT(11),
-    cli_pf_data_nascimento INT(11),
+    cli_pj_telefone	VARCHAR(255),
+    cli_pj_ie VARCHAR(255),
     cli_pj_responsavel VARCHAR(255),	
-    cli_pj_resp_telefone INT(11),
+    cli_pj_resp_telefone VARCHAR(255),
+    cli_cpf	VARCHAR(255),
+    cli_pf_nome VARCHAR(255),
+    cli_pf_telefone VARCHAR(255),
+    cli_pf_data_nascimento VARCHAR(255),
     cli_endereco VARCHAR(255),
-    cli_numero INT(11),
+    cli_numero VARCHAR(255),
     cli_complemento VARCHAR(255),
     cli_bairro VARCHAR(255),
     cli_cidade VARCHAR(255),
     cli_estado VARCHAR(255),	
-    cli_cep INT(11),
+    cli_cep VARCHAR(255),
     cli_status VARCHAR(255),
     cli_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
-CREATE TABLE clientes_log(
-    log_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    log_cli_id INT(11),
-    log_cli_cnpj INT(11),
-    log_cli_motorista VARCHAR(255),
-    log_cli_motor_telefone INT(11),
+CREATE TABLE motoristas(
+    mot_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    mot_cli_id INT(11) NOT NULL ,
+    mot_nome VARCHAR(255),
+    mot_telefone VARCHAR(255),
+    CONSTRAINT fk_cli_mot FOREIGN KEY (mot_cli_id) REFERENCES clientes (cli_id)
+);
+CREATE TABLE veiculos(
+    vei_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    vei_cli_id INT(11) NOT NULL,
+    vei_placa VARCHAR(255),
+    vei_veiculo VARCHAR(255),
+    CONSTRAINT fk_cli_vei FOREIGN KEY (vei_cli_id) REFERENCES clientes (cli_id)
+);
+
+CREATE TABLE log_clientes(
+    log_cli_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    log_cli_cnpj VARCHAR(255),
     log_cli_pj_razao_social	VARCHAR(255),
-    log_cli_pj_telefone	INT(11),
-    log_cli_pj_ie INT(11),
-    log_cli_cpf	INT(11),
-    log_cli_pf_data_nascimento INT(11),
+    log_cli_pj_telefone	VARCHAR(255),
+    log_cli_pj_ie VARCHAR(255),
     log_cli_pj_responsavel VARCHAR(255),	
-    log_cli_pj_resp_telefone INT(11),
+    log_cli_pj_resp_telefone VARCHAR(255),
+    log_cli_cpf	VARCHAR(255),
+    log_cli_pf_nome VARCHAR(255),
+    log_cli_pf_telefone VARCHAR(255),
+    log_cli_pf_data_nascimento VARCHAR(255),
     log_cli_endereco VARCHAR(255),
-    log_cli_numero INT(11),
+    log_cli_numero VARCHAR(255),
     log_cli_complemento VARCHAR(255),
     log_cli_bairro VARCHAR(255),
     log_cli_cidade VARCHAR(255),
     log_cli_estado VARCHAR(255),	
-    log_cli_cep INT(11),
+    log_cli_cep VARCHAR(255),
     log_cli_status VARCHAR(255),
     log_cli_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
-DESCRIBE games;
+);
+
+CREATE TABLE log_motoristas(
+    log_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    log_mot_id INT(11) NOT NULL,
+    log_mot_cli_id INT(11),
+    log_mot_nome VARCHAR(255),
+    log_mot_telefone VARCHAR(255),
+    CONSTRAINT log_fk_cli_mot FOREIGN KEY (log_mot_cli_id) REFERENCES log_clientes (cli_id)
+);
+CREATE TABLE log_veiculos(
+    log_id INT(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    log_vei_id INT(11) NOT NULL,
+    log_vei_cli_id INT(11),
+    log_vei_placa VARCHAR(255),
+    log_vei_veiculo VARCHAR(255),
+    CONSTRAINT log_fk_cli_vei FOREIGN KEY (log_vei_cli_id) REFERENCES log_clientes (cli_id)
+);
